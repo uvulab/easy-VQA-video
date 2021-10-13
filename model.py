2e41fb0b80bae8e2d1306cfd71729c2cf10fed98
arhencey <arhencey@yahoo.com>
2021-10-13 18:11:59 -0400

    changed kernel sizes

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, Conv3D, MaxPooling3D, Flatten, Multiply
+from tensorflow.keras.layers import Input, Dense, Conv3D, MaxPooling3D, Flatten, Multiply, Dropout
 from tensorflow.keras.optimizers import Adam
 
 def build_model(vid_shape, vocab_size, num_answers, big_model):
@@ -14,6 +14,7 @@ def build_model(vid_shape, vocab_size, num_answers, big_model):
     x1 = MaxPooling3D(padding='same')(x1)
   x1 = Flatten()(x1)
   x1 = Dense(32, activation='tanh')(x1)
+  x1 = Dropout(0.4)(x1)
 
   # The question network
   q_input = Input(shape=(vocab_size,))
