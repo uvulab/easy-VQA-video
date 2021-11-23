ba6d1f37c5563b42b9bc50dc60c3f9db927abd75
arhencey <arhencey@yahoo.com>
2021-11-23 18:38:05 -0500

    fixed typo

--- objAttModel/CHECK_SCORE.py
+++ objAttModel/CHECK_SCORE.py
@@ -12,7 +12,7 @@ def check_scores(filename='model.h5'):
     #model = build_model(vid_shape, vocab_size, num_answers, args.big_model)
     model = load_model(filename)
     model.summary()
-    score1_extractor = Model(inputs=model.input, outputs=model.get_layer('scoring_model')get_layer('score_output').output)
+    score1_extractor = Model(inputs=model.input, outputs=model.get_layer('scoring_model').get_layer('score_output').output)
     #score2_extractor = Model(inputs=model.input, outputs=model.get_layer('v2_score').output)
     scores1 = score1_extractor.predict([test_X_first_objects, test_X_second_objects, test_X_seqs])
     #scores2 = score1_extractor.predict([test_X_first_objects, test_X_second_objects, test_X_seqs])
